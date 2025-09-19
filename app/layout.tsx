c8a79d431c07f44914aad73ff48e7326b823fad8
Daniel Alejandro Datri <daniel.datri@gmail.com>
2025-09-19 19:18:25 -0300

    feat: integrate Google Tag Manager for enhanced tracking and analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,6 +49,16 @@ export default function RootLayout({
     <html lang="es">
       <head>
         <meta name="robots" content="index, follow" />
+        {/* Google Tag Manager */}
+        <Script id="gtm-head">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NRBSDTCB');
+          `}
+        </Script>
         {/* Script de Google AdSense */}
         <Script
           async
@@ -73,6 +83,11 @@ export default function RootLayout({
         )}
       </head>
       <body className={`font-sans ${inter.variable} ${jetbrainsMono.variable} ${playfair.variable} antialiased`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NRBSDTCB"
+            height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe>
+        </noscript>
         <HeaderBlock />
         <div style={{ height: '104px' }} />
         <div>
